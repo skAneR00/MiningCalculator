14c71faae26377b256dc607851df805c3f989953
Yuriy <Alimskoy@gmail.com>
2022-03-30 01:07:27 +0600

    FirstCommit

---
+++ HakatonApplication/DataBaseController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HakatonApplication
+{
+    public class DataBaseController
+    {
+        #region Path
+        //Переменные с путями для нахождения файла с данными (Необходимо будет изменить при загрузке на другой ПК) (Если не поменяете, то по идее не будет работать, но это не точно)
+        public static string pathToPUDataBase = @"C:\\Users\Vladimir\source\repos\HakatonApplication\HakatonApplication\БД Хакатон\БД Блок питания.txt";
+        public static string pathToGPUDataBase = @"C:\\Users\Vladimir\source\repos\HakatonApplication\HakatonApplication\БД Хакатон\БД видеокарты.txt";
+        public static string pathToHDDDataBase = @"C:\\Users\Vladimir\source\repos\HakatonApplication\HakatonApplication\БД Хакатон\БД жекстий диск.txt";
+        public static string pathToRAMDataBase = @"C:\\Users\Vladimir\source\repos\HakatonApplication\HakatonApplication\БД Хакатон\БД ОЗУ.txt";
+        public static string pathToCPUDataBase = @"C:\\Users\Vladimir\source\repos\HakatonApplication\HakatonApplication\БД Хакатон\БД процессоры.txt";
+        #endregion
+
+        #region FileInfo
+        //Нужны для проверки существования файла
+        FileInfo filePUInf = new FileInfo(pathToPUDataBase); 
+        FileInfo fileGPUInf = new FileInfo(pathToGPUDataBase);
+        FileInfo fileHDDInf = new FileInfo(pathToHDDDataBase);
+        FileInfo fileRAMInf = new FileInfo(pathToRAMDataBase);
+        FileInfo fileCPUInf = new FileInfo(pathToCPUDataBase);
+        #endregion
+
+        #region PUDataBase
+        public static string[] formattedList; //Массив с отсортированными данными из файла Блоков питания
+        public Queue<String> PUNamesQueue = new Queue<String>(); //Очередь нужна для временного хранения названий блоков питания
+        public static string[] PUNamesReady; //Готовый массив с именами блоков питания
+        #endregion
+        #region GPUDataBase
+        public static string[] formattedGPUList; //Массив с отсортированными данными из файла Видеокарт
+        public Queue<String> GPUNamesQueue = new Queue<String>(); //Очередь нужна для временного хранения названий видеокарт
+        public static string[] GPUNamesReady; //Готовый массив с именами видеокарт
+        #endregion
+        #region HDDDataBase
+        public static string[] formattedHDDList; //Массив с отсортированными данными из файла жестких дисков
+        public Queue<String> HDDNamesQueue = new Queue<String>(); //Очередь нужна для временного хранения названий жестких дисков
+        public static string[] HDDNamesReady; //Готовый массив с именами жестких дисков
+        #endregion
+        #region RAMDataBase
+        public static string[] formattedRAMList; //Массив с отсортированными данными из файла Оперативной памяти
+        public Queue<String> RAMNamesQueue = new Queue<String>(); //Очередь нужна для временного хранения названий оперативной памяти
+        public static string[] RAMNamesReady; //Готовый массив с именами оперативной памяти
+        #endregion
+        #region CPUDataBase
+        public static string[] formattedCPUList; //Массив с отсортированными данными из файла процессоров
+        public Queue<String> CPUNamesQueue = new Queue<String>(); //Очередь нужна для временного хранения названий процессоров
+        public static string[] CPUNamesReady; //Готовый массив с именами процессоров
+        #endregion
+
+
+        //Функиця для работы с блоками питания
+        public void ShowPUDetails()
+        {
+            if (filePUInf.Exists)
+            {
+                StreamReader reader = new StreamReader(pathToPUDataBase); //Нужен для построчного чтения файла
+                string compList = reader.ReadToEnd(); //Считывает файл
+                formattedList = compList.Split( new char[] {',','\r','\n'}, StringSplitOptions.RemoveEmptyEntries ); //Форматирует массив, убирает из него пробелы между строк 
+                
+                foreach(var val in formattedList) //перебор отсортированного массива
+                {
+                    if(val.Length > 10) //условия для получения имен
+                    {
+                        PUNamesQueue.Enqueue(val); //Добавление имени в Очередь
+                    }
+                }
+
+                PUNamesReady = PUNamesQueue.ToArray(); //Копирует очередь в массив с именами
+                
+                reader.Close(); //Завершает считывание файла
+            }
+        }
+
+
+        //Функция для работы с видеокартами
+        public void ShowGPUDetails()
+        {
+            if (fileGPUInf.Exists)
+            {
+                StreamReader reader = new StreamReader(pathToGPUDataBase); //Нужен для построчного чтения файла
+                string compList = reader.ReadToEnd(); //Считывает файл
+                formattedGPUList = compList.Split(new char[] { ',', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); //Форматирует массив, убирает из него пробелы между строк 
+
+                foreach (var val in formattedGPUList) //перебор отсортированного массива
+                {
+                    if (val.Length > 10) //условия для получения имен
+                    {
+                        GPUNamesQueue.Enqueue(val); //Добавление имени в Очередь
+                    }
+                }
+
+                GPUNamesReady = GPUNamesQueue.ToArray(); //Копирует очередь в массив с именами
+
+                reader.Close(); //Завершает считывание файла
+            }
+        }
+
+
+        //Функция для работы с процессорами
+        public void ShowСPUDetails()
+        {
+            if (fileCPUInf.Exists)
+            {
+                StreamReader reader = new StreamReader(pathToCPUDataBase); //Нужен для построчного чтения файла
+                string compList = reader.ReadToEnd(); //Считывает файл
+                formattedCPUList = compList.Split(new char[] { ',', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); //Форматирует массив, убирает из него пробелы между строк 
+
+                foreach (var val in formattedCPUList) //перебор отсортированного массива
+                {
+                    if (val.Length > 10) //условия для получения имен
+                    {
+                        CPUNamesQueue.Enqueue(val); //Добавление имени в Очередь
+                    }
+                }
+
+                CPUNamesReady = CPUNamesQueue.ToArray(); //Копирует очередь в массив с именами
+
+                reader.Close(); //Завершает считывание файла
+            }
+        }
+
+
+        //Функция для работы с оперативной памятью
+        public void ShowRAMDetails()
+        {
+            if (fileRAMInf.Exists)
+            {
+                StreamReader reader = new StreamReader(pathToRAMDataBase); //Нужен для построчного чтения файла
+                string compList = reader.ReadToEnd(); //Считывает файл
+                formattedRAMList = compList.Split(new char[] { ',', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); //Форматирует массив, убирает из него пробелы между строк 
+
+                foreach (var val in formattedRAMList) //перебор отсортированного массива
+                {
+                    if (val.Length > 10) //условия для получения имен
+                    {
+                        RAMNamesQueue.Enqueue(val); //Добавление имени в Очередь
+                    }
+                }
+
+                RAMNamesReady = RAMNamesQueue.ToArray(); //Копирует очередь в массив с именами
+
+                reader.Close(); //Завершает считывание файла
+            }
+        }
+
+
+        //Функция для работы с жесткими дисками
+        public void ShowHDDDetails()
+        {
+            if (fileHDDInf.Exists)
+            {
+                StreamReader reader = new StreamReader(pathToHDDDataBase); //Нужен для построчного чтения файла
+                string compList = reader.ReadToEnd(); //Считывает файл
+                formattedHDDList = compList.Split(new char[] { ',', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); //Форматирует массив, убирает из него пробелы между строк 
+
+                foreach (var val in formattedHDDList) //перебор отсортированного массива
+                {
+                    if (val.Length > 10) //условия для получения имен
+                    {
+                        HDDNamesQueue.Enqueue(val); //Добавление имени в Очередь
+                    }
+                }
+
+                HDDNamesReady = HDDNamesQueue.ToArray(); //Копирует очередь в массив с именами
+
+                reader.Close(); //Завершает считывание файла
+            }
+        }
+    }
+}
---
+++ HakatonApplication/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+using NLog;
+
+namespace HakatonApplication
+{
+    public partial class Form1 : Form
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        //Переменные для часовых доходов/расходов
+        public double ProfitRUB = 0;
+        public double ProfitTNG = 0;
+        public double ProfitUSD = 0;
+        public double EnergyTNG = 0;
+        public double EnergyUSD = 0;
+        public double EnergyRUB = 0;
+
+
+        //Функции для расчета дохода/расхода за час/день/месяц/год
+        private void Calculations()
+        {
+            double deviceCount = Convert.ToDouble(numericUpDown1.Value);
+            //Функция, которая вычисляет доход/расходы за час для каждого типа устройств отдельно
+            if (DataBaseController.formattedList.Contains(comboBox2.Text))
+            {
+                int index = Array.IndexOf(DataBaseController.formattedList, comboBox2.Text);
+                double PUEnergy = Convert.ToDouble(DataBaseController.formattedList[index + 1]);
+                double PUProfit = Convert.ToDouble(DataBaseController.formattedList[index + 2]);
+                ProfitUSD = PUProfit * deviceCount;
+                ProfitTNG = ProfitUSD * 500 * deviceCount;
+                ProfitRUB = ProfitUSD * 100 * deviceCount;
+                EnergyTNG = PUEnergy * 6.5 * deviceCount;
+                EnergyRUB = EnergyTNG / 5 * deviceCount;
+                EnergyUSD = EnergyTNG / 500 * deviceCount;
+                Console.WriteLine("Energy {0}. Profit {1}",EnergyTNG, ProfitTNG);
+                
+            }
+            else if (DataBaseController.formattedCPUList.Contains(comboBox2.Text))
+            {
+                int index = Array.IndexOf(DataBaseController.formattedCPUList, comboBox2.Text);
+                double CPUEnergy = Convert.ToDouble(DataBaseController.formattedCPUList[index + 1]);
+                double CPUProfit = Convert.ToDouble(DataBaseController.formattedCPUList[index + 2]);
+                ProfitUSD = CPUProfit * deviceCount;
+                ProfitTNG = ProfitUSD * 500 * deviceCount;
+                ProfitRUB = ProfitUSD * 100 * deviceCount;
+                EnergyTNG = CPUEnergy * 6.5 * deviceCount;
+                EnergyRUB = EnergyTNG / 5 * deviceCount;
+                EnergyUSD = EnergyTNG / 500 * deviceCount;
+                Console.WriteLine("Energy {0}. Profit {1}", EnergyTNG, ProfitTNG);
+            }
+            else if (DataBaseController.formattedGPUList.Contains(comboBox2.Text))
+            {
+                int index = Array.IndexOf(DataBaseController.formattedGPUList, comboBox2.Text);
+                double GPUEnergy = Convert.ToDouble(DataBaseController.formattedGPUList[index + 1]);
+                double GPUProfit = Convert.ToDouble(DataBaseController.formattedGPUList[index + 2]);
+                ProfitUSD = GPUProfit * deviceCount;
+                ProfitTNG = ProfitUSD * 500 * deviceCount;
+                ProfitRUB = ProfitUSD * 100 * deviceCount;
+                EnergyTNG = GPUEnergy * 6.5 * deviceCount;
+                EnergyRUB = EnergyTNG / 5 * deviceCount;
+                EnergyUSD = EnergyTNG / 500 * deviceCount;
+                Console.WriteLine("Energy {0}. Profit {1}", EnergyTNG, ProfitTNG);
+            }
+            else if (DataBaseController.formattedRAMList.Contains(comboBox2.Text))
+            {
+                int index = Array.IndexOf(DataBaseController.formattedRAMList, comboBox2.Text);
+                double RAMEnergy = Convert.ToDouble(DataBaseController.formattedRAMList[index + 1]);
+                double RAMProfit = Convert.ToDouble(DataBaseController.formattedRAMList[index + 2]);
+                ProfitUSD = RAMProfit * deviceCount;
+                ProfitTNG = ProfitUSD * 500 * deviceCount;
+                ProfitRUB = ProfitUSD * 100 * deviceCount;
+                EnergyTNG = RAMEnergy * 6.5 * deviceCount;
+                EnergyRUB = EnergyTNG / 5 * deviceCount;
+                EnergyUSD = EnergyTNG / 500 * deviceCount;
+                Console.WriteLine("Energy {0}. Profit {1}", EnergyTNG, ProfitTNG);
+            }
+            else if (DataBaseController.formattedHDDList.Contains(comboBox2.Text))
+            {
+                int index = Array.IndexOf(DataBaseController.formattedHDDList, comboBox2.Text);
+                double HDDEnergy = Convert.ToDouble(DataBaseController.formattedHDDList[index + 1]);
+                double HDDProfit = Convert.ToDouble(DataBaseController.formattedHDDList[index + 2]);
+                ProfitUSD = HDDProfit;
+                ProfitTNG = ProfitUSD * 500;
+                ProfitRUB = ProfitUSD * 100;
+                EnergyTNG = HDDEnergy * 6.5;
+                EnergyRUB = EnergyTNG / 5;
+                EnergyUSD = EnergyTNG / 500;
+                Console.WriteLine("Energy {0}. Profit {1}", EnergyTNG, ProfitTNG);
+            }
+            MessageBox.Show("Доход с данного устройства в час: " +
+                "\n" + $"В рублях: {ProfitRUB} " +
+                "\n" + $"В тенге: {ProfitTNG} " +
+                "\n" + $"В долларах: {ProfitUSD} " + "\n" +
+                "\n" + "Расходы на энергию с данного устройства в час: " +
+                "\n" + $"В рублях: {EnergyRUB}" +
+                "\n" + $"В тенге: {EnergyTNG}" +
+                "\n" + $"В долларах: {EnergyUSD}" + "\n" +
+                "\n" + "Чистая прибыль: " +
+                "\n" + $"В рублях: {ProfitRUB - EnergyRUB}" +
+                "\n" + $"В тенге: {ProfitTNG - EnergyTNG}" +
+                "\n" + $"В долларах: {ProfitUSD - EnergyUSD}");
+        }
+
+
+        private void CalculationDay()
+        {
+            double deviceCount = Convert.ToDouble(numericUpDown1.Value);
+            double DayProfitRUB = ProfitRUB * 24 * deviceCount;
+            double DayProfitTNG = ProfitTNG * 24 * deviceCount;
+            double DayProfitUSD = ProfitUSD * 24 * deviceCount;
+            double DayEnergyTNG = EnergyTNG * 24 * deviceCount;
+            double DayEnergyUSD = EnergyUSD * 24 * deviceCount;
+            double DayEnergyRUB = EnergyRUB * 24 * deviceCount;
+            MessageBox.Show("Доход с данного устройства в день: " +
+                "\n" + $"В рублях: {DayProfitRUB} " +
+                "\n" + $"В тенге: {DayProfitTNG} " +
+                "\n" + $"В долларах: {DayProfitUSD} " + "\n" +
+                "\n" + "Расходы на энергию с данного устройства в день: " + 
+                "\n" + $"В рублях: {DayEnergyRUB}" + 
+                "\n" + $"В тенге: {DayEnergyTNG}" + 
+                "\n" + $"В долларах: {DayEnergyUSD}" + "\n" +
+                "\n" + "Чистая прибыль: " + 
+                "\n" + $"В рублях: {DayProfitRUB-DayEnergyRUB}" +
+                "\n" + $"В тенге: {DayProfitTNG-DayEnergyTNG}" + 
+                "\n" + $"В долларах: {DayProfitUSD-DayEnergyUSD}");
+        }
+
+
+        private void CalculationMonth()
+        {
+            double deviceCount = Convert.ToDouble(numericUpDown1.Value);
+            double MonthProfitRUB = ProfitRUB * 730 * deviceCount;
+            double MonthProfitTNG = ProfitTNG * 730 * deviceCount;
+            double MonthProfitUSD = ProfitUSD * 730 * deviceCount;
+            double MonthEnergyTNG = EnergyTNG * 730 * deviceCount;
+            double MonthEnergyUSD = EnergyUSD * 730 * deviceCount;
+            double MonthEnergyRUB = EnergyRUB * 730 * deviceCount;
+            MessageBox.Show("Доход с данного устройства в месяц: " +
+                "\n" + $"В рублях: {MonthProfitRUB} " +
+                "\n" + $"В тенге: {MonthProfitTNG} " +
+                "\n" + $"В долларах: {MonthProfitUSD} " + "\n" +
+                "\n" + "Расходы на энергию с данного устройства в месяц: " +
+                "\n" + $"В рублях: {MonthEnergyRUB}" +
+                "\n" + $"В тенге: {MonthEnergyTNG}" +
+                "\n" + $"В долларах: {MonthEnergyUSD}" + "\n" + 
+                "\n" + "Чистая прибыль: " +
+                "\n" + $"В рублях: {MonthProfitRUB - MonthEnergyRUB}" +
+                "\n" + $"В тенге: {MonthProfitTNG - MonthEnergyTNG}" +
+                "\n" + $"В долларах: {MonthProfitUSD - MonthEnergyUSD}");
+        }
+
+
+        private void CalculationYear()
+        {
+            double deviceCount = Convert.ToDouble(numericUpDown1.Value);
+            double YearProfitRUB = ProfitRUB * 8760 * deviceCount;
+            double YearProfitTNG = ProfitTNG * 8760 * deviceCount;
+            double YearProfitUSD = ProfitUSD * 8760 * deviceCount;
+            double YearEnergyTNG = EnergyTNG * 8760 * deviceCount;
+            double YearEnergyUSD = EnergyUSD * 8760 * deviceCount;
+            double YearEnergyRUB = EnergyRUB * 8760 * deviceCount;
+            MessageBox.Show("Доход с данного устройства в год: " +
+                "\n" + $"В рублях: {YearProfitRUB} " +
+                "\n" + $"В тенге: {YearProfitTNG} " +
+                "\n" + $"В долларах: {YearProfitUSD} " + "\n" + 
+                "\n" + "Расходы на энергию с данного устройства в год: " +
+                "\n" + $"В рублях: {YearEnergyRUB}" +
+                "\n" + $"В тенге: {YearEnergyTNG}" +
+                "\n" + $"В долларах: {YearEnergyUSD}" + "\n" + 
+                "\n" + "Чистая прибыль: " +
+                "\n" + $"В рублях: {YearProfitRUB - YearEnergyRUB}" +
+                "\n" + $"В тенге: {YearProfitTNG - YearEnergyTNG}" +
+                "\n" + $"В долларах: {YearProfitUSD - YearEnergyUSD}");
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            comboBox1.Items.AddRange(new string[] { "us USD - $", "ru RUB - " + "\x20BD", "kz TNG - \x20B8" });
+            comboBox3.Items.AddRange(new string[] { "Блоки питания", "Видеокарты", "Жесткие диски",
+                "Оперативная память", "Процессоры"});
+            logger.Debug("Валюты, типы устройств были загружены");
+        }
+
+        //Событие загрузки формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logger.Debug("Была запущена форма");
+            DataBaseController DBController = new DataBaseController();
+            DBController.ShowPUDetails();
+            DBController.ShowGPUDetails();
+            DBController.ShowСPUDetails();
+            DBController.ShowRAMDetails();
+            DBController.ShowHDDDetails();
+        }
+
+        //Событие изменения текста в боксе "Валюта"
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    textBox1.Text = EnergyUSD + "              " + "USD/kWH";
+                    textBox2.Text = ProfitUSD + "              " + "USD";
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    textBox1.Text = EnergyRUB + "             " + "RUB/kWH";
+                    textBox2.Text = ProfitRUB + "           " + "RUB";
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    textBox1.Text = EnergyTNG + "             " + "TNG/kWH";
+                    textBox2.Text = ProfitTNG + "           " + "TNG";
+                }
+                logger.Debug("Валюта была изменена пользователем");
+            }
+        }
+
+        //Кнопка для расчета
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculations();
+            CalculationDay();
+            CalculationMonth();
+            CalculationYear();
+            logger.Debug("Пользователь нажал на кнопку расчета");
+        }
+
+        //Событие изменения текста в боксе "Выберите тип устройства"
+        private void comboBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text != null)
+            {
+                //Проверки на тип выбранного устройства
+                if (comboBox3.Text == "Блоки питания")
+                {
+                    comboBox2.Items.Clear();
+                    foreach(var item in DataBaseController.PUNamesReady)
+                    {
+                        comboBox2.Items.Add(item);
+                    }
+                    logger.Debug("Пользователь выбрал тип устройства: Блоки питания");
+                }
+                else if (comboBox3.Text == "Видеокарты")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (var item in DataBaseController.GPUNamesReady)
+                    {
+                        comboBox2.Items.Add(item);
+                    }
+                    logger.Debug("Пользователь выбрал тип устройства: Видеокарты");
+                }
+                else if (comboBox3.Text == "Жесткие диски")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (var item in DataBaseController.HDDNamesReady)
+                    {
+                        comboBox2.Items.Add(item);
+                    }
+                    logger.Debug("Пользователь выбрал тип устройства: Жесткие диски");
+                }
+                else if (comboBox3.Text == "Оперативная память")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (var item in DataBaseController.RAMNamesReady)
+                    {
+                        comboBox2.Items.Add(item);
+                    }
+                    logger.Debug("Пользователь выбрал тип устройства: Оперативная память");
+                }
+                else if (comboBox3.Text == "Процессоры")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (var item in DataBaseController.CPUNamesReady)
+                    {
+                        comboBox2.Items.Add(item);
+                    }
+                    logger.Debug("Пользователь выбрал тип устройства: Процессоры");
+                }
+            }
+        }
+
+        //Постоянное обновление формы
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    textBox1.Text = EnergyUSD + "              " + "USD/kWH";
+                    textBox2.Text = ProfitUSD + "              " + "USD";
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    textBox1.Text = EnergyRUB + "             " + "RUB/kWH";
+                    textBox2.Text = ProfitRUB + "           " + "RUB";
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    textBox1.Text = EnergyTNG + "             " + "TNG/kWH";
+                    textBox2.Text = ProfitTNG + "           " + "TNG";
+                }
+            }
+        }
+    }
+}
